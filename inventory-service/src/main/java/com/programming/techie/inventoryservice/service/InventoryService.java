d17212095401d719700d57966f95630e77a10931
Kajal <kajalshanu24@gmail.com>
2024-11-04 21:12:52 +0530

    Order_Service_App and partial inventory_Service_App

---
+++ inventory-service/src/main/java/com/programming/techie/inventoryservice/service/InventoryService.java
@@ -0,0 +1,18 @@
+package com.programming.techie.inventoryservice.service;
+
+import com.programming.techie.inventoryservice.repository.InventoryRepository;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode) {
+        return inventoryRepository.findBySkuCode().isPresent();
+    }
+}
