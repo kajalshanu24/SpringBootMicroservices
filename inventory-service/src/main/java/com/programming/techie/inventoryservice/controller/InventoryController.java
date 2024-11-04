790b0e33f496eca113568e5a08971e6a7485eb10
Kajal <kajalshanu24@gmail.com>
2024-11-04 22:03:36 +0530

    Sync call between Inventory_Services and Order_Service

--- inventory-service/src/main/java/com/programming/techie/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/programming/techie/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.programming.techie.inventoryservice.controller;
 
+import com.programming.techie.inventoryservice.dto.InventoryResponse;
 import com.programming.techie.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -12,9 +15,12 @@ public class InventoryController {
 
     private final InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    //http://localhost:8082/api/inventory/iphone-13-green,iphone-13-blue
+    //OR
+    //http://localhost:8082/api/inventory?skuCode=iphone-13-green&skuCode=iphone-13-blue
+    @GetMapping()
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
         return inventoryService.isInStock(skuCode);
     }
 }
--- inventory-service/src/main/java/com/programming/techie/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/programming/techie/inventoryservice/service/InventoryService.java
@@ -1,9 +1,15 @@
 package com.programming.techie.inventoryservice.service;
 
+import com.programming.techie.inventoryservice.dto.InventoryResponse;
+import com.programming.techie.inventoryservice.model.Inventory;
 import com.programming.techie.inventoryservice.repository.InventoryRepository;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 @RequiredArgsConstructor
@@ -12,7 +18,12 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream().map(inventory ->
+            InventoryResponse.builder()
+                    .skuCode(inventory.getSkuCode())
+                    .isInStock(inventory.getQuantity() > 0)
+                    .build()
+        ).toList();
     }
 }
