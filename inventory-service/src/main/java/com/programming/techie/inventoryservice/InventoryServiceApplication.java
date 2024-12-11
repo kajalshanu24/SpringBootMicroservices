a7b3f1caf7ecbcb7d37fdb3e8d5cb0468c492332
Kajal <kajalshanu24@gmail.com>
2024-12-11 18:51:20 +0530

    Added ApiGateway changes and resolving IDE issues

--- inventory-service/src/main/java/com/programming/techie/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/programming/techie/inventoryservice/InventoryServiceApplication.java
@@ -6,7 +6,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -33,4 +39,19 @@ public class InventoryServiceApplication {
             inventoryRepository.save(inventory1);
         };
     }
+
+    /*@Bean
+    public EurekaInstanceConfigBean eurekaInstanceConfigBean(Environment env) throws UnknownHostException {
+        EurekaInstanceConfigBean bean = new EurekaInstanceConfigBean(env);
+        bean.setInstanceId(instanceId(env));
+        return bean;
+    }
+
+    private String instanceId(Environment env) throws UnknownHostException {
+        String appName = env.getProperty("spring.application.name");
+        String hostname = InetAddress.getLocalHost().getHostName();
+        String ipAddress = InetAddress.getLocalHost().getHostAddress();
+        int port = env.getProperty("server.port", Integer.class, 0);
+        return appName + ":" + hostname + ":" + ipAddress + ":" + port;
+    }*/
 }
